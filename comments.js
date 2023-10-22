2e35504b0d3a7778c0276cddd69421ee66d61fee
Chickenn <76015523+Hxhnchxn@users.noreply.github.com>
2023-10-22 14:48:30 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,58 @@
+//create web server
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+var fs = require('fs');
+var cors = require('cors');
+app.use(cors());
+var comments = require('./comments.json');
+var path = require('path');
+var port = 3000;
+
+//test
+app.get('/', function(req, res) {
+    res.send('Hello World!');
+});
+
+//get all comments
+app.get('/comments', function(req, res) {
+    res.json(comments);
+});
+
+//get comment by id
+app.get('/comments/:id', function(req, res) {
+    var id = req.params.id;
+    var comment = comments.find(comment => comment.id == id);
+    res.json(comment);
+});
+
+//post new comment
+app.post('/comments', function(req, res) {
+    var comment = req.body;
+    comments.push(comment);
+    res.json(comment);
+});
+
+//update comment by id
+app.put('/comments/:id', function(req, res) {
+    var id = req.params.id;
+    var comment = comments.find(comment => comment.id == id);
+    comment.name = req.body.name;
+    comment.email = req.body.email;
+    comment.comment = req.body.comment;
+    res.json(comment);
+});
+
+//delete comment by id
+app.delete('/comments/:id', function(req, res) {
+    var id = req.params.id;
+    var comment = comments.find(comment => comment.id == id);
+    comments.splice(comments.indexOf(comment), 1);
+    res.json(comment);
+});
+
+//start server
+app.listen(port, function() {
+    console.log('Example app listening on port ' + port + '!');
+});
